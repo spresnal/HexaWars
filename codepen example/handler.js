4b48ba9c03cc284c993035fb4052938d2785afc0
Sam Presnal <spresnal@purdue.edu>
2016-02-14 17:00:53 -0600

    Added angular seed project

---
+++ codepen example/handler.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function (handler, $, undefined) {
+    var canvas;
+
+    $(document).ready(function () {
+        var canvas = new Canvas();
+    });
+
+}(window.handler = window.handler || {}, jQuery));
