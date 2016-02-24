669905235a1da1bc82766bfce4722a4e95a3b1c6
Hsieh11 <Chrishsieh@live.com>
2016-02-23 22:46:25 -0500

    Created file to highlight hexagons, editing event listener

--- client/app/home/gameboard-service.js
+++ client/app/home/gameboard-service.js
@@ -57,25 +57,7 @@ angular.module('myApp.home').service('GameboardService', function () {
 
             drawBoard(ctx, boardWidth, boardHeight);
 
-            canvas.addEventListener('click', function (eventInfo) {
-                var x = eventInfo.offsetX || eventInfo.layerX,
-                    y = eventInfo.offsetY || eventInfo.layerY,
-                    hexY = Math.floor(y / (hexHeight + sideLength)),
-                    hexX = Math.floor((x - (hexY % 2) * hexRadius) / hexRectangleWidth),
-                    screenX = hexX * hexRectangleWidth + ((hexY % 2) * hexRadius),
-                    screenY = hexY * (hexHeight + sideLength);
-
-                drawBoard(ctx, boardWidth, boardHeight);
-
-                // Check if the mouse's coords are on the board
-                if (hexX >= 0 && hexX < boardWidth) {
-                    if (hexY >= 0 && hexY < boardHeight) {
-                        ctx.fillStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
-
-                        drawHexagon(ctx, screenX, screenY, true);
-                    }
-                }
-            });
+            canvas.addEventListener('click', selectHexagon-service.highlightHexagon());
         }
     }
 });
---
+++ client/app/home/selectHexagon-service.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+
+angular.module('myApp.home').service('SelectHexagonService', function() {
+
+    this.highlightHexagon = (function(eventInfo) {
+        var x = eventInfo.offsetX || eventInfo.layerX,
+            y = eventInfo.offsetY || eventInfo.layerY,
+            hexY = Math.floor(y / (hexHeight + sideLength)),
+            hexX = Math.floor((x - (hexY % 2) * hexRadius) / hexRectangleWidth),
+            screenX = hexX * hexRectangleWidth + ((hexY % 2) * hexRadius),
+            screenY = hexY * (hexHeight + sideLength);
+
+        // drawBoard(ctx, boardWidth, boardHeight);
+
+        // Check if the mouse's coords are on the board
+        if (hexX >= 0 && hexX < boardWidth) {
+            if (hexY >= 0 && hexY < boardHeight) {
+                ctx.fillStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+                drawHexagon(ctx, screenX, screenY, true);
+            }
+        }
+    });
+});
